21e9a5f1bf544e0488e161db990b415c9e577535
esarnb <esarnb@gmail.com>
2021-05-13 23:44:10 -0400

    removed admin lock temporarily, added short error handlebar, fixed user controller sending sorted name isNotEnabled

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const { getRoutes } = require('../utils');
 const homeController = require('../controllers/home-controller');
-const userController = require('../controllers/user-controller');
+// const userController = require('../controllers/user-controller');
 
 module.exports = (logger) => {
   router.get('/', homeController.index);
   router.get('/login', homeController.login);
-  router.get('/user', userController.userEnabled);
+  // router.get('/user', userController.userEnabled);
   router.use(getRoutes(logger, __dirname, __filename));
   return router;
 };
