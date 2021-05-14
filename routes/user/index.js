7d3d0c78345afffeb1babc4f8a674e1d18c10b49
esarnb <esarnb@gmail.com>
2021-05-14 00:26:27 -0400

    isAdmin worked! Added back the middleware check

--- routes/user/index.js
+++ routes/user/index.js
@@ -3,6 +3,6 @@ const mustBeLoggedIn = require('../../middleware/auth/must-be-logged-in');
 const mustBeAdmin = require('../../middleware/auth/must-be-admin');
 const userController = require('../../controllers/user-controller');
 
-routes.get('/not-is-enabled', mustBeLoggedIn, userController.getNotIsEnabled);
+routes.get('/not-is-enabled', mustBeLoggedIn, mustBeAdmin, userController.getNotIsEnabled);
 
 module.exports = () => routes;
