e207d584a72183e1b1616f07394a4a4d6606abe8
esarnb <esarnb@gmail.com>
2021-05-14 13:10:36 -0400

    added client fetch route for manage user btns

--- public/scripts/users/manage-users.js
+++ public/scripts/users/manage-users.js
@@ -19,21 +19,74 @@ const disableUser = async (event) => {
   const userid = event.target;
   console.log(userid);
 
-  fetch(`/manage/disable/${userid}`)
-    .then((response) => {
-      if (response.ok) {
-        document.location.replace('/users/manage');
-      }
-      return response.json();
-    })
-    .then(({ message }) => {
-      error.form.textContent = message;
-    })
-    .catch(() => {
-      error.form.textContent = 'An error occurred while attempting to save your profile';
-    });
+  fetch('/manage/disable', {
+    method: 'PUT',
+    body: JSON.stringify({ id: userid }),
+    headers: { 'Content-Type': 'application/json' },
+  }).then((response) => {
+    if (response.ok) {
+      document.location.replace('/users/manage');
+    }
+    return response.json();
+  }).then(({ message }) => {
+    error.form.textContent = message;
+  }).catch(() => {
+    error.form.textContent = 'An error occurred while attempting to save your profile';
+  });
 };
 
-document.querySelector('.1').addEventListener('click', disableUser);
-document.querySelector('.2').addEventListener('click', enableUser);
-document.querySelector('.3').addEventListener('click', deleteUser);
+const enableUser = async (event) => {
+  const error = {
+    form: document.querySelector('#manage-error'),
+  };
+
+  clearErrors(error);
+
+  const userid = event.target;
+  console.log(userid);
+
+  fetch('/manage/enable', {
+    method: 'PUT',
+    body: JSON.stringify({ id: userid }),
+    headers: { 'Content-Type': 'application/json' },
+  }).then((response) => {
+    if (response.ok) {
+      document.location.replace('/users/manage');
+    }
+    return response.json();
+  }).then(({ message }) => {
+    error.form.textContent = message;
+  }).catch(() => {
+    error.form.textContent = 'An error occurred while attempting to save your profile';
+  });
+};
+
+const deleteUser = async (event) => {
+  const error = {
+    form: document.querySelector('#manage-error'),
+  };
+
+  clearErrors(error);
+
+  const userid = event.target;
+  console.log(userid);
+
+  fetch('/manage/delete', {
+    method: 'DELETE',
+    body: JSON.stringify({ id: userid }),
+    headers: { 'Content-Type': 'application/json' },
+  }).then((response) => {
+    if (response.ok) {
+      document.location.replace('/users/manage');
+    }
+    return response.json();
+  }).then(({ message }) => {
+    error.form.textContent = message;
+  }).catch(() => {
+    error.form.textContent = 'An error occurred while attempting to save your profile';
+  });
+};
+
+document.querySelector('.enable-btn').addEventListener('click', enableUser);
+document.querySelector('.disable-btn').addEventListener('click', disableUser);
+document.querySelector('.delete-btn').addEventListener('click', deleteUser);
