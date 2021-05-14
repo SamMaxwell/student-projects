59a6bd635d3647da4a6899bc154da88391078a12
esarnb <esarnb@gmail.com>
2021-05-13 22:56:08 -0400

    add userController route in routes/index.js, routes/user folder + route with middleware security  and path to isAdmin

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const { getRoutes } = require('../utils');
 const homeController = require('../controllers/home-controller');
+const userController = require('../controllers/user-controller');
 
 module.exports = (logger) => {
   router.get('/', homeController.index);
   router.get('/login', homeController.login);
+  router.get('/not-is-enabled', userController.userEnabled);
   router.use(getRoutes(logger, __dirname, __filename));
   return router;
 };
